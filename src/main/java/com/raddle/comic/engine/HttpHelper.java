8e81ecdfc2194a1b6fcebc6a50026db34d8cf6f2
raddle60 <raddle60@gmail.com>
2013-12-06 13:57:02 +0000

    获得页面

---
+++ src/main/java/com/raddle/comic/engine/HttpHelper.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.raddle.comic.engine;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @author raddle
+ * 
+ */
+public class HttpHelper {
+	public String getRemotePage(String url, String charset) throws IOException {
+		CloseableHttpClient httpclient = HttpClients.createMinimal();
+		HttpGet httpGet = new HttpGet(url);
+		CloseableHttpResponse response = httpclient.execute(httpGet);
+		try {
+			HttpEntity entity1 = response.getEntity();
+			if (response.getStatusLine().getStatusCode() == 200) {
+				return EntityUtils.toString(entity1, charset);
+			} else {
+				EntityUtils.consume(entity1);
+				throw new RuntimeException("获得内容失败:" + response.getStatusLine());
+			}
+		} finally {
+			response.close();
+		}
+	}
+}
---
+++ src/main/java/com/raddle/comic/engine/PageInfo.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.raddle.comic.engine;
+
+/**
+ * @author raddle
+ * 
+ */
+public class PageInfo {
+	private Integer pageNo;
+	private String pageUrl;
+
+	public Integer getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(Integer pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public String getPageUrl() {
+		return pageUrl;
+	}
+
+	public void setPageUrl(String pageUrl) {
+		this.pageUrl = pageUrl;
+	}
+}
