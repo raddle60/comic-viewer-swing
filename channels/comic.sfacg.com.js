78299676d31bf29110294c9f332ced90615abe58
raddle60 <raddle60@gmail.com>
2013-12-08 06:04:34 +0000

    漫画来源

---
+++ channels/comic.sfacg.com.js
@@ -0,0 +1,79 @@
+/**
+ * 来源渠道描述
+ */
+var channel = {
+	name : "SF互动传媒网",
+	home : "http://comic.sfacg.com/",
+	desc : "http://comic.sfacg.com/HTML/OnePiece/730/\nOnePiece是漫画Id\n730是章节Id\n"+
+	"http://comic.sfacg.com/HTML/YJDWB/SP/SP1/\nYJDWB是漫画Id\nSP/SP1是章节Id"
+}
+/**
+ * 获得动漫的名称
+ * 
+ * @param comicId
+ */
+function getComic(comicId) {
+	return null;
+}
+
+/**
+ * 获得章节列表
+ * 
+ * @param comicId
+ */
+function getSections(comicId) {
+	var content = httpclient.getRemotePage("http://comic.sfacg.com/HTML/" + comicId + "/", "utf-8", {});
+	var sectionRegex =  new RegExp("<li><a href=\"/HTML/"+comicId+"/[^/]+/\" target=\"_blank\">(([^<>]+)|(<font color=red>([^<>]+)</font>))</a></li>","g");
+	var matched = content.match(sectionRegex);
+	var sections = [];
+	if(matched != null && matched.length > 0){
+		for ( var i = 0; i < matched.length; i++) {
+			var sectionId = matched[i].match(new RegExp("/" + comicId + "/(\\w+)/"))[1];
+			var sectionName = matched[i].match(/>([^<>]+)</)[1];
+			sections.push({
+				sectionId : sectionId,
+				name : sectionName
+			});
+		}
+	}
+	var extSectionRegex =  new RegExp("<li><a href=\"/HTML/"+comicId+"/[^/]+/[^/]+/\" target=\"_blank\">(([^<>]+)|(<font color=red>([^<>]+)</font>))</a></li>","g");
+	var extMatched = content.match(extSectionRegex);
+	if(extMatched != null && extMatched.length > 0){
+		for ( var i = 0; i < extMatched.length; i++) {
+			var sectionId = extMatched[i].match(new RegExp("/" + comicId + "/(\\w+/\\w+)/"))[1];
+			var sectionName = extMatched[i].match(/>([^<>]+)</)[1];
+			sections.push({
+				sectionId : sectionId,
+				name : sectionName
+			});
+		}
+	}
+	return sections.reverse();
+}
+
+/**
+ * 获得章节的页数
+ * 
+ * @param SectionId
+ */
+function getPages(comicId, sectionId) {
+	var content = httpclient.getRemotePage("http://comic.sfacg.com/HTML/" + comicId + "/" + sectionId + "/", "utf-8", {});
+	var jsUrl = content.match(/\/Utility\/\w+\/(\w+\/)?\w+.js/);
+	if(jsUrl != null && jsUrl.length > 0){
+		var contentjs = httpclient.getRemotePage("http://comic.sfacg.com/" +jsUrl[0] , "utf-8", {});
+		engine.eval(contentjs);
+		var pages = [];
+		for ( var i = 0; i < picAy.length; i++) {
+			pages[i] = {
+				pageNo : i + 1,
+				pageUrl : hosts[0] + picAy[i]
+			}
+		}
+		return pages;
+	}
+	return null;
+}
+
+function loadRemoteImage(comicId, sectionId, imageUrl) {
+	httpclient.saveRemoteImage(channel.name,comicId,sectionId,imageUrl,{});
+}
