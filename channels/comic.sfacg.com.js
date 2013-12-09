5dc2112ee4f3623bfd01cc7b6a0143abf1a89879
raddle60 <raddle60@gmail.com>
2013-12-09 12:19:19 +0000

    eval返回结果对象

--- channels/comic.sfacg.com.js
+++ channels/comic.sfacg.com.js
@@ -53,12 +53,12 @@ function getPages(comicId, sectionId) {
 	var jsUrl = content.match(/\/Utility\/\w+\/(\w+\/)?\w+.js/);
 	if(jsUrl != null && jsUrl.length > 0){
 		var contentjs = httpclient.getRemotePage("http://comic.sfacg.com/" +jsUrl[0] , "utf-8", {});
-		engine.eval(contentjs);
+		var evalResult = engine.eval(contentjs);
 		var pages = [];
-		for ( var i = 0; i < picAy.length; i++) {
+		for ( var i = 0; i < evalResult.picAy.length; i++) {
 			pages[i] = {
 				pageNo : i + 1,
-				pageUrl : hosts[0] + picAy[i]
+				pageUrl : evalResult.hosts[0] + evalResult.picAy[i]
 			}
 		}
 		return pages;
