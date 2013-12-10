48162028e3243d0631c0a0139743e97723c55237
raddle60@gmail.com <raddle60@gmail.com@26e01157-914a-1682-3cac-f5e6a7cf52f4>
2013-12-10 13:28:32 +0000

    使用统一的实例

--- trunk/src/main/java/com/raddle/comic/engine/HttpHelper.java
+++ trunk/src/main/java/com/raddle/comic/engine/HttpHelper.java
@@ -15,6 +15,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
@@ -23,41 +24,42 @@ import org.apache.http.util.EntityUtils;
  * 
  */
 public class HttpHelper {
+	private final static CloseableHttpClient httpclient;
+	static {
+		HttpClientBuilder custom = HttpClients.custom();
+		custom.setUserAgent("Mozilla/5.0 (Windows NT 6.1; rv:25.0) Gecko/20100101 Firefox/25.0");
+		httpclient = custom.build();
+	}
+
 	public static String getRemotePage(String url, String charset, Map<Object, Object> headers) throws IOException {
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		try {
-			HttpGet httpGet = new HttpGet(url);
-			if (headers != null) {
-				for (Map.Entry<Object, Object> entry : headers.entrySet()) {
-					httpGet.addHeader(entry.getKey() + "", entry.getValue() + "");
-				}
+		HttpGet httpGet = new HttpGet(url);
+		if (headers != null) {
+			for (Map.Entry<Object, Object> entry : headers.entrySet()) {
+				httpGet.addHeader(entry.getKey() + "", entry.getValue() + "");
 			}
-			CloseableHttpResponse response = httpclient.execute(httpGet);
-			try {
-				HttpEntity entity1 = response.getEntity();
-				if (response.getStatusLine().getStatusCode() == 200) {
-					return EntityUtils.toString(entity1, charset);
-				} else {
-					EntityUtils.consume(entity1);
-					throw new RuntimeException("获得内容失败:" + response.getStatusLine());
-				}
-			} finally {
-				response.close();
+		}
+		CloseableHttpResponse response = httpclient.execute(httpGet);
+		try {
+			HttpEntity entity1 = response.getEntity();
+			if (response.getStatusLine().getStatusCode() == 200) {
+				return EntityUtils.toString(entity1, charset);
+			} else {
+				EntityUtils.consume(entity1);
+				throw new RuntimeException("获得内容失败:" + response.getStatusLine());
 			}
 		} finally {
-			httpclient.close();
+			response.close();
 		}
 	}
 
-	public static void saveRemoteImage(String channel, String comicId, String sectionId, String imageUrl, String cacheFileName, Map<Object, Object> headers)
-			throws IOException {
+	public static void saveRemoteImage(String channel, String comicId, String sectionId, String imageUrl, String cacheFileName,
+			Map<Object, Object> headers) throws IOException {
 		File cacheFile = new File(System.getProperty("user.home") + "/.comic-view/cache/img/" + channel + "/" + comicId + "/" + sectionId + "/"
 				+ FilenameUtils.getName(imageUrl));
 		saveRemotePage(imageUrl, headers, cacheFile);
 	}
 
 	public static void saveRemotePage(String url, Map<Object, Object> headers, File file) throws IOException {
-		CloseableHttpClient httpclient = HttpClients.createMinimal();
 		HttpGet httpGet = new HttpGet(url);
 		if (headers != null) {
 			for (Map.Entry<Object, Object> entry : headers.entrySet()) {
